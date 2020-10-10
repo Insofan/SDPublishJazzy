a565eb0ed0e28b669d11bac30e270ff35454ac04
inso <insofan3156@gmail.com>
2020-10-10 14:10:33 +0800

    add: add gen jazzy

--- main.js
+++ main.js
@@ -1,37 +1,43 @@
-const core = require("@actions/core")
-const github = require("@actions/github")
-const shell = require("shelljs")
-const yaml = require("js-yaml")
-const fs = require("fs")
+const core = require("@actions/core");
+const github = require("@actions/github");
+const shell = require("shelljs");
+const yaml = require("js-yaml");
+const fs = require("fs");
 
-const context = github.context
+const context = github.context;
 
-const branch = "docs"
+const branch = "docs";
 
 // User defined input
-const jazzyVersion = core.getInput("version")
-const configFilePath = core.getInput("config")
-const jazzyArgs = core.getInput("args")
-const token = core.getInput("personal_access_token")
+const configPath = core.getInput("config_path");
+const jazzyVersion = core.getInput("jazzy_version");
+const token = core.getInput("personal_access_token");
 
-const remote = `https://${token}@github.com/${context.repo.owner}/${context.repo.repo}.git`
-
+const remote = `https://${token}@github.com/${context.repo.owner}/${context.repo.repo}.git`;
 
 const installJazzy = () => {
-    let str = "sudo gem install jazzy"
+  let str = "sudo gem install jazzy";
 
-    if (jazzyVersion) {
-        str += `-v ${jazzyVersion}`
-    }
-    return str
-}
+  if (jazzyVersion) {
+    str += `-v ${jazzyVersion}`;
+  }
+  return str;
+};
+
+const genJazzy = () => {
+  if (configPath) {
+    return "jazzy --config .jazzy.yaml";
+  }
+  return "jazzy";
+};
 
 const execute = () => {
-    shell.exec(installJazzy())
-}
+  shell.exec(installJazzy());
+  shell.exec(genJazzy());
+};
 
 try {
-    execute()
-}catch (e){
-    core.setFailed(e.message)
+  execute();
+} catch (e) {
+  core.setFailed(e.message);
 }
